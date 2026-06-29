1ceccdd02208b698575f60faf89e6264f9d1ffd5
Kirti1110 <kirtiv.1998@gmail.com>
2026-06-29 16:13:02 +1000

    Update PR 1.py

--- PR 1.py
+++ PR 1.py
@@ -12,8 +12,6 @@ print(f"Age: {age} (Type: {type(age)}, Memory Address: {id(age)})")
 print(f"Height: {height} (Type: {type(height)}, Memory Address: {id(height)})")
 print(f"Favourite Number: {favourite_number} (Type: {type(favourite_number)}, Memory Address: {id(favourite_number)})")
 
-from datetime import datetime
-
 current_year = datetime.now().year
 birth_year = current_year - age
 
